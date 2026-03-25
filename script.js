16f0e60fd215638cfcb7281825b8410290185e79
Hd11Patil <harshal.dp11@gmail.com>
2026-03-25 08:00:14 +0530

    next

--- script.js
+++ script.js
@@ -78,7 +78,7 @@
   });
 })();
 
-// ----------------------------------------------------------------------
+// ── Slider Logic ──
 let currentSlideIndex = 0;
 const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
